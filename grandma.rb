b2724b9e403dd4175e5d76114b9d6b9c70d1a67e
Lizzie Chan <lchan217@gmail.com>
2019-01-28 22:18:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase) 
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.tolowercase
+  elsif phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
   else 
     puts "NO, NOT SINCE 1983!"
